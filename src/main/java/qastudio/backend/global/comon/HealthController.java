83ec0485c905298052ed4fa95225b5d70f196d77
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-01-10 19:04:39 +0900

    a

--- src/main/java/qastudio/backend/global/comon/HealthController.java
+++ src/main/java/qastudio/backend/global/comon/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Tag(name = "헬스 체킹 Controller")
+@Tag(name = "헬스 체킹 Controllers")
 public class HealthController {
 
     @GetMapping("/health")
